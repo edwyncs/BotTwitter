63e2e382827e7efeb673f13f95536d204b785a4d
edwyncs <edwyn_cs@hotmail.com>
2021-06-07 09:14:46 -0500

    se cambio el index

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -1,13 +1,6 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "Hello, " + name + ". This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized.";
-
+    context.log('salida en el log');
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: "Hola mundo"
     };
-}
+};
